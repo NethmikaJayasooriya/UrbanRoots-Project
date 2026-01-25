372b530a24aed357b531730695769859f664539c
NethmikaJayasooriya <work.nethmika@gmail.com>
2026-01-25 11:58:12 +0530

    chore: setup database env variables and config

--- backend_api/src/app.module.ts
+++ backend_api/src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres', // Default username
-      password: 'MSI', 
-      database: 'urbanroots_db',
-      entities: [],
-      synchronize: true,    // Auto-creates tables
+    //Load the Config Module first
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes .env available everywhere
+    }),
+
+    // 2. Connect to Database using the variables
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [], // Add your entities here later
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
