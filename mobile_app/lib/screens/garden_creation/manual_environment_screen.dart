ca84d3b9b1bcc2ba91edc9b35a0696496d406d19
NethmikaJayasooriya <work.nethmika@gmail.com>
2026-02-11 09:16:43 +0530

    Initialize variable and soil option data creation map

--- mobile_app/lib/screens/garden_creation/manual_environment_screen.dart
+++ mobile_app/lib/screens/garden_creation/manual_environment_screen.dart
@@ -8,3 +8,18 @@ class ManualEnvironmentScreen extends StatefulWidget {
   State<ManualEnvironmentScreen> createState() => _ManualEnvironmentScreenState();
 }
 
+class _ManualEnvironmentScreenState extends State<ManualEnvironmentScreen> {
+  // State variables
+  String _selectedSoil = "Potting Mix";
+  double _sunlightValue = 50; 
+  String _wateringFrequency = "Daily";
+  bool _isWindy = false; //variable for high rise balconies
+
+  // Soil Options Data
+  final List<Map<String, String>> _soilTypes = [
+    {"name": "Potting Mix", "desc": "High Nutrients, Balanced"},
+    {"name": "Red Soil", "desc": "Garden Soil, Acidic"},
+    {"name": "Sandy / Beach", "desc": "Low Nutrients, Drains Well"},
+    {"name": "Compost", "desc": "Organic, High Nitrogen"},
+  ];
+}
