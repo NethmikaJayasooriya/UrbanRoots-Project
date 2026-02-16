fcf9017114006806533bdd9fb06cba8b2ef731a2
Nethmi Divakara <nethmidivakara@.com>
2026-02-16 15:29:00 +0530

    changind main to add other routes

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'auth/splash_screen.dart'; // import your splash screen
+import 'package:splashscreen/auth/create_new_pass.dart';
+import 'package:splashscreen/auth/forgot_pass.dart';
+import 'package:splashscreen/auth/verification_page.dart';
+import 'auth/login.dart';
+import 'auth/sign_up.dart';
+import 'auth/get_started.dart';
+import 'auth/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +18,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Login Pages',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const SplashScreen(), 
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const GetStarted(),
+        '/splash_screen': (context) => const SplashScreen(),
+        '/login':(context)=> const LoginScreen(),
+        '/sign_up': (context) => SignUpScreen(),
+        '/forgot_pass' : (context)=>ForgotPasswordApp(),
+        '/verification_page' : (context)=>VerificationPage(),
+        '/create_new_pass': (context)=>CreateNewPass(),
+
+      },
     );
   }
 }
