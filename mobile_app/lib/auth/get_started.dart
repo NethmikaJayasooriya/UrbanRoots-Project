dbf47c504d0492852fd73ef542892688551af1f1
Nethmi Divakara <nethmidivakara@.com>
2026-02-17 22:58:24 +0530

    removed the get started button and fixed it with a buffer with 4 sec to redirect to the other page

--- mobile_app/lib/auth/get_started.dart
+++ mobile_app/lib/auth/get_started.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart';
 
-class GetStarted extends StatelessWidget {
+class GetStarted extends StatefulWidget {
   const GetStarted({super.key});
 
   @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  void initState() {
+    super.initState();
+    _redirectToSplash();
+  }
+  Future<void> _redirectToSplash() async {
+    await Future.delayed(const Duration(seconds: 4));
+
+    if (!mounted) return;
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SplashScreen(),
+      ),
+    );
+  }
+
+
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0A1F1A),
+      backgroundColor: const Color(0xFF0F1115),
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
@@ -14,46 +40,31 @@ class GetStarted extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset(
-                  'assets/logo.png',
-                  height: 450,
-                  width: 529,
-                  fit: BoxFit.contain,
+                // Logo with slight upward offset
+                Transform.translate(
+                  offset: const Offset(-20, -45),
+                  child: Image.asset(
+                    'assets/logo.png',
+                    height: 529,
+                    width: 629,
+                    fit: BoxFit.contain,
+                  ),
                 ),
-                const SizedBox(height: 50), // reduced space
 
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/splash_screen');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF161917),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      side: const BorderSide(
-                        color: Color(0xFF4ADE80),
-                        width: 2, // optional: adjust border thickness
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 80,
-                      vertical: 16,
-                    ),
-                  ),
-                  child: const Text(
-                    'Get Started',
-                    style: TextStyle(
-                      color: Color(0xFF4ADE80),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
+                const SizedBox(height: 180), // space below logo
+                //  Spinner
+                const CircularProgressIndicator(
+                  strokeWidth: 4,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Color(0xFF00FF9D),
                   ),
                 ),
+
               ],
             ),
           ),
         ),
       ),
     );
-  }
+}
 }
