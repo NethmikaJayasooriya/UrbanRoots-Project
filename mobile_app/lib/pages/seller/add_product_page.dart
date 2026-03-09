7c049fe2548390296f94cf6f1c4ee42de1e9b1ad
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-03-09 12:24:06 +0530

    page for seller to add a product to the marketplace

---
+++ mobile_app/lib/pages/seller/add_product_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:mobile_app/models/products.dart';
+
+class AddProductPage extends StatefulWidget {
+  const AddProductPage({super.key});
+  @override
+  State<AddProductPage> createState() => _State();
+}
