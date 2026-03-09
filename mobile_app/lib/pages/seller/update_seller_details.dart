7eebb5cc7ee837566c358a865c3b5bdb1f6904df
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-03-10 01:04:48 +0530

    added a page to allow sellers to update and edit there details

---
+++ mobile_app/lib/pages/seller/update_seller_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:mobile_app/models/beneficiary.dart';
+
+
+
+
+class UpdateSellerDetailsPage extends StatefulWidget {
+  const UpdateSellerDetailsPage({super.key});
+  @override
+  State<UpdateSellerDetailsPage> createState() => _State();
+}
