aba32ec944b38c1627ac72f7bb92e44fe75ba4f4
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-03-10 00:49:09 +0530

    products overview page for the seller

---
+++ mobile_app/lib/pages/seller/seller_products_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/models/products.dart';
+
+
+class SellerProductsPage extends StatefulWidget {
+  const SellerProductsPage({super.key});
+  @override
+  State<SellerProductsPage> createState() => _State();
+}
