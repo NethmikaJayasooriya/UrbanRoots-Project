a086da16065c44cccc5ceb929852be99fe7b6252
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-03-09 12:37:06 +0530

    added a sales page for sellers to view purchaces/transaction history

---
+++ mobile_app/lib/pages/seller/sales_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/models/products.dart';
+import 'package:mobile_app/models/sale.dart';
+
+
+class SalesPage extends StatefulWidget {
+  const SalesPage({super.key});
+  @override
+  State<SalesPage> createState() => _State();
+}
