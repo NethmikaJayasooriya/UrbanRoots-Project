81c8f3a6cc4793fff51c944ebc7ab6591c83448e
Thevinu Gunawardena <thevinu.rivertune@gmail.com>
2026-03-15 19:23:45 +0530

    Made cartmodel accessible through multiple widgets

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
-import 'marketplace/marketplaceScreen1.dart'; 
-
-
+import 'package:provider/provider.dart';
+import 'package:mobile_app/marketplace/marketplaceScreen1.dart';
+import 'package:mobile_app/marketplace/cart_model.dart';
 
 void main() {
-
-  runApp(const UrbanRootsApp());
-
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CartModel(),
+      child: const UrbanRootsApp(),
+    ),
+  );
 }
 
-
-
 class UrbanRootsApp extends StatelessWidget {
-
   const UrbanRootsApp({super.key});
 
-
-
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       title: 'UrbanRoots',
-
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
-
         primarySwatch: Colors.green,
-
         scaffoldBackgroundColor: const Color(0xFFF8F9FA),
-
         useMaterial3: true,
-
       ),
-
       home: const MarketplaceScreen1(),
-
     );
-
   }
-
 }
