c06d9deaa89cd8371d1b39912f06f4ed7cbd4340
shaneobby <163554908+shaneobby@users.noreply.github.com>
2026-03-21 22:22:28 +0530

    user interactions

---
+++ backend_api/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repo: Repository<User>,
+  ) {}
+
+  /** Mark a user as a seller after onboarding */
+  async markAsSeller(uid: string): Promise<void> {
+    await this.repo.update({ uid }, { is_seller: true, is_onboarded: true });
+  }
+
+  async findOne(uid: string): Promise<User | null> {
+    return this.repo.findOneBy({ uid });
+  }
+}
